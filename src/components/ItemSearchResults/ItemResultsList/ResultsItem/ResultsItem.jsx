b68040617b450de104167f9cbcc28b52ae866b35
Chris Nelson <chrisnelson@MacBook-Pro-2.local>
2021-12-18 14:15:51 -0600

    select item from search results working

--- src/components/ItemSearchResults/ItemResultsList/ResultsItem/ResultsItem.jsx
+++ src/components/ItemSearchResults/ItemResultsList/ResultsItem/ResultsItem.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
 function resultsItem(props) {
+  //set dispatch
+  const dispatch = useDispatch()
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Results Item');
 
+  const selectItem = () => {
+    dispatch({type:'SET_ACTIVE_ITEM', payload:props.item})
+  }
   return (
     <div className='component'>
       <h2>{heading}</h2>
-      <p>{JSON.stringify(props)}</p>
+      <p>{JSON.stringify(props.item)}</p>
+      <Link to="/item_info">
+      <button onClick={selectItem}>Select Item</button>
+      </Link>
     </div>
   );
 }
