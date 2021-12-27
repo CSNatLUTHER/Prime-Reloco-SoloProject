b9264c06181899f83ea101bded3a537829e3b0e1
Chris Nelson <chrisnelson@chris.local>
2021-12-26 21:14:28 -0600

    GET box and box search now include box destination data

--- server/routes/box.router.js
+++ server/routes/box.router.js
@@ -7,7 +7,11 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   console.log(req.query);
-  const query = `SELECT * FROM box
+  const query = `SELECT box.id, box.qr_id, box.name, box.create_date, 
+                box.creator_user_id, box.last_update_date, box.last_modified_user_id, 
+                box.event_id, box.size, box.weight, box.destination_id, destination.destination 
+                FROM box
+                JOIN destination ON box.destination_id=destination.id
                 WHERE event_id=${req.query.event}
                 ORDER BY "id" ASC`;
   pool.query(query)
@@ -40,7 +44,11 @@ router.get('/box-items', (req, res) => {
 router.get('/search', (req, res) => {
   console.log('in search with:', req.query);
   // GET route code here
-    const query = `SELECT * FROM box
+    const query = `SELECT box.id, box.qr_id, box.name, box.create_date, 
+                   box.creator_user_id, box.last_update_date, box.last_modified_user_id, 
+                   box.event_id, box.size, box.weight, box.destination_id, destination.destination 
+                   FROM box
+                   JOIN destination ON box.destination_id=destination.id
                    WHERE (box.name ILIKE '%${req.query.searchText}%' AND box.event_id=${req.query.event})
                    OR (box.qr_id ILIKE '%${req.query.searchText}%' AND box.event_id=${req.query.event})
                    ORDER BY "create_date" DESC;`
