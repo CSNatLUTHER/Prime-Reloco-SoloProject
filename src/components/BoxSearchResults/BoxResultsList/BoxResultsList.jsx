060f457508fe927328fb6f7d1156ddf6200a5f86
Chris Nelson <chrisnelson@chris.local>
2022-01-03 20:54:20 -0600

    application now handles search results with no results found

--- src/components/BoxSearchResults/BoxResultsList/BoxResultsList.jsx
+++ src/components/BoxSearchResults/BoxResultsList/BoxResultsList.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import ResultsBox from '../BoxResultsList/ResultsBox/ResultsBox'
+import './BoxResultsList.css'
+import { useHistory } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -12,11 +16,26 @@ function boxResultsList(props) {
   const [heading, setHeading] = useState('Box Results List');
   const boxResults = useSelector((store) => store.boxes);
 
+  const history = useHistory();
+  
   return (
     <div className='component'>
       {/* <h2>{heading}</h2> */}
       {/* <h4>Search Results: {JSON.stringify(boxResults)}</h4> */}
-      {boxResults.map(box => (<ResultsBox className="resultsBox" box={box} key={box.id} />))}
+      {boxResults.length === 0 ?
+        <>
+          <br />
+          <h2>NO RESULTS FOUND</h2>
+          <br />
+          <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => { setTimeout(() => history.push('/move_event_home'), 250) }}>RETURN TO SEARCH</Button>
+        </>:
+        <>
+          <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => { setTimeout(() => history.push('/move_event_home'), 250) }}>RETURN TO SEARCH</Button>
+          <br />
+          <br />
+          {boxResults.map(box => (<ResultsBox className="resultsBox" box={box} key={box.id} />))}
+        </>
+      }
       {/* <ResultsBox /> */}
     </div>
   );
--- src/components/ItemSearchResults/ItemResultsList/ItemResultsList.jsx
+++ src/components/ItemSearchResults/ItemResultsList/ItemResultsList.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 import ResultsItem from './ResultsItem/ResultsItem';
+import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import './ItemResultsList.css';
+import { useHistory } from 'react-router-dom';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -12,11 +16,24 @@ function itemResultsList(props) {
   const itemList = useSelector((store) => store.items);
   const [heading, setHeading] = useState('Item Results List');
 
+  const history = useHistory();
+
   return (
     <div className='component'>
-      {/* <h2>{heading}</h2> */}
-      {itemList.map(item => (<ResultsItem className="ResultsItem" item={item} key={item.id} />))}
-      {/* <ResultsItem /> */}
+      {itemList.length === 0?
+        <>
+        <br />
+        <h2>NO RESULTS FOUND</h2>
+        <br />
+        <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => {setTimeout(()=>history.push('/move_event_home'), 250)}}>RETURN TO SEARCH</Button>
+        </>:
+        <>
+        <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => {setTimeout(()=>history.push('/move_event_home'), 250)}}>RETURN TO SEARCH</Button>
+        <br />
+        <br />
+        {itemList.map(item => (<ResultsItem className="ResultsItem" item={item} key={item.id} />))}
+        </>
+      }
     </div>
   );
 }
