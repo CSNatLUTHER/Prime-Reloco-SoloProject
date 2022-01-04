18e01c68de834d1eb58d865527012dff114ff622
Chris Nelson <chrisnelson@chris.local>
2022-01-03 20:57:48 -0600

    time for code clean-up and comments

--- src/components/BoxSearchResults/BoxResultsList/BoxResultsList.jsx
+++ src/components/BoxSearchResults/BoxResultsList/BoxResultsList.jsx
@@ -6,29 +6,22 @@ import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
+
 function boxResultsList(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Box Results List');
   const boxResults = useSelector((store) => store.boxes);
-
   const history = useHistory();
-  
+
   return (
     <div className='component'>
-      {/* <h2>{heading}</h2> */}
-      {/* <h4>Search Results: {JSON.stringify(boxResults)}</h4> */}
       {boxResults.length === 0 ?
         <>
           <br />
           <h2>NO RESULTS FOUND</h2>
           <br />
           <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => { setTimeout(() => history.push('/move_event_home'), 250) }}>RETURN TO SEARCH</Button>
-        </>:
+        </> :
         <>
           <Button color="secondary" variant="contained" className='searchItemBackButton' startIcon={<ArrowBackIosIcon />} onClick={() => { setTimeout(() => history.push('/move_event_home'), 250) }}>RETURN TO SEARCH</Button>
           <br />
@@ -36,7 +29,6 @@ function boxResultsList(props) {
           {boxResults.map(box => (<ResultsBox className="resultsBox" box={box} key={box.id} />))}
         </>
       }
-      {/* <ResultsBox /> */}
     </div>
   );
 }
--- src/components/ItemSearchResults/ItemResultsList/ItemResultsList.jsx
+++ src/components/ItemSearchResults/ItemResultsList/ItemResultsList.jsx
@@ -6,16 +6,10 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import './ItemResultsList.css';
 import { useHistory } from 'react-router-dom';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
 function itemResultsList(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const itemList = useSelector((store) => store.items);
   const [heading, setHeading] = useState('Item Results List');
-
   const history = useHistory();
 
   return (
